485b05266dc772b8d304df494492cefed41f7f35
JosephCrespin <67466171+JosephCrespin@users.noreply.github.com>
2021-05-10 19:20:29 +0200

    images done

--- src/components/Projects/ProjectList/data.jsx
+++ src/components/Projects/ProjectList/data.jsx
@@ -1,14 +1,26 @@
+import img from '../../../images/projects/infinite.png';
 
 const data = [
   {
-    title: 'Infinite Blocks',
+    title: 'InfiniteBlocks',
     technologies: ['html5', 'css3', 'sass', 'js'],
     description:
       'Project based on simon game where you have to follow patterns and remember them to win',
-    image: require('../../../images/projects/infinite.png'),
+    image:img,
     site: 'https://josephcrespin.github.io/InfiniteBlocks/',
     source: 'https://github.com/JosephCrespin/InfiniteBlocks',
   },
+   {
+    title: 'InfiniteBlocks',
+    technologies: ['html5', 'css3', 'sass', 'js'],
+    description:
+      'Project based on simon game where you have to follow patterns and remember them to win',
+    image:img,
+    site: 'https://josephcrespin.github.io/InfiniteBlocks/',
+    source: 'https://github.com/JosephCrespin/InfiniteBlocks',
+  },
+  
+
   
 ];
 
